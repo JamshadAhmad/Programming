618cb33e2e83d6b5177f95d2432130ea2ead3f9e
JamshadAhmad <jamshad.ahmad@coeus-solutions.de>
2020-04-17 06:04:15 +0500

    is valid BST

---
+++ LeetCode Problems/Validate BST.php
@@ -0,0 +1,38 @@
+<?php
+
+// https://leetcode.com/problems/validate-binary-search-tree
+
+class TreeNode {
+    public $val = null;
+    public $left = null;
+    public $right = null;
+    function __construct($value) { $this->val = $value; }
+}
+
+/**
+ * @param TreeNode $root
+ * @return Boolean
+ */
+function isValidBST($root) {
+    if ($root === null) { return true;}
+    if ($root->left === null && $root->right === null) {return true;}
+
+    if ($root->left) {
+        if ($root->left->val >= $root->val) { return false;}
+        $current = $root->left;
+        //Biggest value on left side should still be smaller that root <--- Key points
+        while ($current->right !== null) {$current = $current->right;}
+
+        if ($root->val <= $current->val) {return false;}
+    }
+    if ($root->right) {
+        if ($root->right->val <= $root->val) {return false;}
+        $current = $root->right;
+        //smallest value on right side should still be greater than root
+        while ($current->left !== null) {$current = $current->left;}
+
+        if ($root->val >= $current->val) {return false;}
+    }
+
+    return $this->isValidBST($root->left) && $this->isValidBST($root->right);
+}
