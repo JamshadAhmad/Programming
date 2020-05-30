8f14ce5f5f1f76602c879cee0c0a064c56efa875
JamshadAhmad <jamshad.ahmad@coeus-solutions.de>
2020-05-31 04:28:51 +0500

    Longest substring without repeating chars

---
+++ LeetCode Problems/Longest substr without repeating character.php
@@ -0,0 +1,20 @@
+<?php
+
+//https://leetcode.com/problems/longest-substring-without-repeating-characters
+
+function lengthOfLongestSubstring($s) { //O(n) time and space. The inchworm method
+    $l = strlen($s);
+    if ($l < 2) {return $l;}
+    $maxLength = 1;
+    $Indexes = [];
+    $start = 0;
+
+    for ($i = 0; $i < $l; ++$i) {
+        if (array_key_exists($s[$i], $Indexes)) {
+            $start = max($start, $Indexes[$s[$i]] + 1); //move string start to next character of last occurence
+        }
+        $Indexes[$s[$i]] = $i; //store/override last index of each char
+        $maxLength = max($maxLength, $i - $start + 1);
+    }
+    return $maxLength;
+}
